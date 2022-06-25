a612ec4234c71cff94e0043f77e012748f5b1f7e
shreyas <shreyasseshadri@gmail.com>
2022-06-25 18:03:18 +0530

    Moved file storage solution to in-memory (#6)

---
+++ storage/storage.hpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+
+#pragma once
+
+enum StorageType
+{
+    DISK,
+    IN_MEMORY
+};
+
+class Storage
+{
+private:
+    Storage *storage_engine;
+
+public:
+
+    static Storage* factory(StorageType);
+
+    /**
+     * @brief Insert a key value pair
+     * @param key The key
+     * @param value The value
+     */
+    virtual void insert(std::string key, std::string value) = 0;
+
+    /**
+     * @brief Retrieve the value given the key
+     * @param key The key
+     * @return std::string Returns value or Empty String if not found
+     */
+    virtual std::string retrieve(std::string key) = 0;
+};
--- tests/test.cpp
+++ tests/test.cpp
@@ -3,21 +3,21 @@
 #include <string.h>
 #include <assert.h>
 
-#include "../storage/storage-api.hpp"
+#include "../storage/storage.hpp"
 
 int main()
 {
 
-	Storage storage(MAP_INDEXER);
+	Storage* storage = Storage::factory(DISK);
 
 	std::string value = "This is Value1";
-	storage.insert("key1", value);
-	std::string valueRetrieved = storage.retrieve("key1");
+	storage->insert("key1", value);
+	std::string valueRetrieved = storage->retrieve("key1");
 	assert(value == valueRetrieved);
 
 	std::string value2 = "This is Value2";
-	storage.insert("key2", value2);
-	std::string valueRetrieved2 = storage.retrieve("key2");
+	storage->insert("key2", value2);
+	std::string valueRetrieved2 = storage->retrieve("key2");
 	assert(value2 == valueRetrieved2);
 
 	return 0;
